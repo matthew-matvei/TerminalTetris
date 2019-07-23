a2fe9ca49881439be90f60525eeb6377055aba56
Matthew James <matthew.d.james87@gmail.com>
2019-07-23 19:21:51 +0100

    Use observable to keep app running

--- src/TerminalTetris/Program.fs
+++ src/TerminalTetris/Program.fs
@@ -1,9 +1,18 @@
-﻿[<EntryPoint>]
-let main argv =
+﻿open System.Reactive.Linq
+open System
+
+[<EntryPoint>]
+let main _ =
     let exitCode = 0
 
     let (numRows, numColumns) = (10, 5)
     let gameGrid = GameGrid.create numRows numColumns
     GameGrid.render gameGrid |> Draw.matrix
+
+    let mainLoop = Observable.Interval(TimeSpan.FromSeconds(2.0)).Publish()
+    use subscription = mainLoop.Subscribe()
+
+    mainLoop.Connect() |> ignore
+    mainLoop.Wait() |> ignore
 
     exitCode
