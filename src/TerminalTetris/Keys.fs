6f3a4e87caa97e5200ce15b3379955abf75da4f6
matthew-matvei <matthew.d.james87@gmail.com>
2020-02-09 11:28:01 +0000

    Support moving block down on key press

--- src/TerminalTetris/Keys.fs
+++ src/TerminalTetris/Keys.fs
@@ -6,4 +6,5 @@ let applyKeyEvent (key: ConsoleKeyInfo) (gameGrid: GameGrid.Grid) =
     match key.Key with
     | ConsoleKey.RightArrow -> Move.blockRight gameGrid
     | ConsoleKey.LeftArrow -> Move.blockLeft gameGrid
+    | ConsoleKey.DownArrow -> Move.blockDown gameGrid
     | _ -> gameGrid
