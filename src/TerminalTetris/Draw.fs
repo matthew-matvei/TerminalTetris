ba1eef65afd7f483c2367ab00c7ffddb13fa6939
Matthew James <matthew.d.james87@gmail.com>
2020-02-15 11:44:07 +0000

    Do not render border every update (#7)

--- src/TerminalTetris/Draw.fs
+++ src/TerminalTetris/Draw.fs
@@ -2,8 +2,8 @@ module Draw
 
 open System
 
-let matrix (matrix: string[][]) =
-    Console.Clear()
+let matrixAtLocation (location: Location.Location) (matrix: string[][]) =
+    Console.SetCursorPosition(location.X, location.Y)
     for row in matrix do
         for column in row do
             printf "%s" column
--- src/TerminalTetris/Program.fs
+++ src/TerminalTetris/Program.fs
@@ -3,13 +3,19 @@
 [<EntryPoint>]
 let main _ =
     let exitCode = 0
+    
+    Console.Title <- "Terminal Tetris"
+    Console.Clear()
 
     let (numRows, numColumns) = (10, 5)
     let mutable gameGrid = GameGrid.create numRows numColumns
+    let initialCursorLocation = { 
+        Location.X = Console.CursorLeft
+        Location.Y = Console.CursorTop }
 
     let updateGrid (gameGridUpdater) =
         gameGrid <- GameGrid.update gameGrid gameGridUpdater
-        GameGrid.render gameGrid |> Draw.matrix
+        GameGrid.render gameGrid |> Draw.matrixAtLocation initialCursorLocation
 
     GameEngine.run (fun _ -> updateGrid Move.blockDown)
     GameEngine.waitForKey (Keys.applyKeyEvent >> updateGrid)
