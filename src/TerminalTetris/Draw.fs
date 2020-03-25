e0c0a97d10309738c2fdee6b33c4baa283dd1930
Matthew James <matthew.james@zupatech.co.uk>
2020-03-25 17:48:15 +0000

    Initial queue implementation

--- src/TerminalTetris/Draw.fs
+++ src/TerminalTetris/Draw.fs
@@ -1,7 +1,15 @@
 module Draw
 
 open System
+open System.Collections.Concurrent
+
+let private printQueue = new ConcurrentQueue<Location.Location * string>()
 
 let printAt (location: Location.Location) (value: string) =
-    Console.SetCursorPosition(location.X, location.Y)
-    printf "%s" value
+    printQueue.Enqueue((location, value))
+
+    while not printQueue.IsEmpty do
+        let success, (l, v) = printQueue.TryDequeue()
+        if success then
+            Console.SetCursorPosition(l.X, l.Y)
+            printf "%s" v
